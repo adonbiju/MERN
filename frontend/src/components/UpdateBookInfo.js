bdafef4bbdc9c30f224bb10709728a8c79528e16
adonbiju <adonbiju@gmail.com>
2022-08-03 20:41:46 +0530

    frontend update book page design completed

--- frontend/src/components/UpdateBookInfo.js
+++ frontend/src/components/UpdateBookInfo.js
@@ -1,8 +1,101 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import '../App.css';
 
 function UpdateBookInfo() {
   return (
-    <div>UpdateBookInfo</div>
+    <div className="UpdateBookInfo">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <br />
+              <Link to="/" className="btn btn-outline-warning float-left">
+                  Show BooK List
+              </Link>
+            </div>
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Edit Book</h1>
+              <p className="lead text-center">
+                  Update Book's Info
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-8 m-auto">
+          <form >
+            <div className='form-group'>
+              <label htmlFor="title">Title</label>
+              <input
+                type='text'
+                placeholder='Title of the Book'
+                name='title'
+                className='form-control'
+                
+              />
+            </div>
+            <br />
+
+            <div className='form-group'>
+            <label htmlFor="isbn">ISBN</label>
+              <input
+                type='text'
+                placeholder='ISBN'
+                name='isbn'
+                className='form-control'
+              
+              />
+            </div>
+
+            <div className='form-group'>
+            <label htmlFor="author">Author</label>
+              <input
+                type='text'
+                placeholder='Author'
+                name='author'
+                className='form-control'
+             
+              />
+            </div>
+
+            <div className='form-group'>
+            <label htmlFor="description">Description</label>
+              <input
+                type='text'
+                placeholder='Describe this book'
+                name='description'
+                className='form-control'
+              
+              />
+            </div>
+
+            <div className='form-group'>
+            <label htmlFor="published_date">Published Date</label>
+              <input
+                type='date'
+                placeholder='published_date'
+                name='published_date'
+                className='form-control'
+                
+              />
+            </div>
+            <div className='form-group'>
+            <label htmlFor="publisher">Publisher</label>
+              <input
+                type='text'
+                placeholder='Publisher of this Book'
+                name='publisher'
+                className='form-control'
+            
+              />
+            </div>
+
+            <button type="submit" className="btn btn-outline-info btn-lg btn-block">Update Book</button>
+            </form>
+          </div>
+
+        </div>
+      </div>
   )
 }
 
