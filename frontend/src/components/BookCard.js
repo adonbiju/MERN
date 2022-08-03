152aa3b9119343660a632db743bbcfa68a3c9aec
adonbiju <adonbiju@gmail.com>
2022-08-03 17:12:49 +0530

    frontend show book page completed

--- frontend/src/components/BookCard.js
+++ frontend/src/components/BookCard.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
-function BookCard() {
-  return (
-    <div>BookCard</div>
-  )
-}
+const BookCard = (props) => {
+    const  book  = props.book;
 
-export default BookCard
+    return(
+        <div className="card-container">
+            <img src="https://th.bing.com/th/id/OIP.CgZ3iqaHmcBhxHOIWWUYMwHaHt?pid=ImgDet&rs=1"width={250} height={200} alt="" />
+            <div className="desc">
+                <h2>
+                    <Link to={`/show-book/${book._id}`}>
+                        { book.title }
+                    </Link>
+                </h2>
+                <h3>{book.author}</h3>
+                <p>{book.description}</p>
+            </div>
+        </div>
+    )
+};
+
+export default BookCard;
