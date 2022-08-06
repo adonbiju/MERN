bc805e3d82dc476a775631560bdd15ef0f09ce96
adonbiju <adonbiju@gmail.com>
2022-08-06 13:05:27 +0530

    updated a readme file

--- frontend/src/components/BookCard.js
+++ frontend/src/components/BookCard.js
@@ -10,7 +10,7 @@ const BookCard = (props) => {
     return(
         <div className="card-container">
            <Link to={`/show-book/${book._id}`}> 
-        <img src="https://cdn.vox-cdn.com/thumbor/p-gGrwlaU4rLikEAgYhupMUhIJc=/0x0:1650x2475/1200x0/filters:focal(0x0:1650x2475):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/13757614/817BsplxI9L.jpg"width={250} height={300} alt="" />
+        <img src="https://m.media-amazon.com/images/I/516Ied32+cL.jpg"width={250} height={300} alt="" />
         </Link>
         <div className="desc">
             <h2>
